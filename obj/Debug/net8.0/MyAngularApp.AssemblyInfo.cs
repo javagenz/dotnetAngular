5785197a2cee35c7e0bf21814dade99a1d9a68f5
javagenz <bonie@MacBook-Pro-Bonie.local>
2024-06-08 19:50:38 +0700

    test

--- obj/Debug/net8.0/MyAngularApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyAngularApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAngularApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe5a69f6f0de064c58d8e57266cee764fcb05b2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17a541bb050f077190955697ae433e235e27102f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAngularApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAngularApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
